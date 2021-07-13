c28ab2ae979b34b130084b818aafd5b9858d3d6a
Evandro-Prog <evandro.presenci@gmail.com>
2021-07-12 21:48:53 -0300

    Inicio da listagem de tarefas.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Filtros } from '../componentes/Filtros';
 import { Footer } from '../componentes/Footer';
 import { Header } from '../componentes/Header';
 import { Listagem } from '../componentes/Listagem';
 
 export const Home = props => {
+
+    const [tarefas, setTarefas] = useState ([
+
+        
+        {
+            id: '2',          
+            nome: 'Estudo Kotlin',
+            dataPrevistaConclusao: '2021-06-23',
+            dataConclusao: '2021-07-10'
+        },
+        {
+            id: '3',            
+            nome: 'Estudo ReactJS',
+            dataPrevistaConclusao: '2021-07-01',
+            dataConclusao: null
+            }
+        
+    ]);
 
     const sair = () => {
         localStorage.removeItem('accessToken');
@@ -17,7 +35,7 @@ export const Home = props => {
         <>
             <Header sair={sair}/>
             <Filtros />      
-            <Listagem />  
+            <Listagem tarefas={tarefas} />  
             <Footer />   
         </>
     )
